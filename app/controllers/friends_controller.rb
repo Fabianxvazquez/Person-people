cb8176fb4e07a25615bfe4af73db40c3824a0f05
Fabian Vazquez <Fabianxvazquez@gmail.com>
2020-02-19 23:37:39 -0700

    updates

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,10 +1,77 @@
 class FriendsController < ApplicationController
+  #before_action :set_blog, :only [show,edit,]
   def index
+    @friends = Friend.all
   end
 
   def show
+    @friend = Friend.find.(params[:id])
   end
 
   def new
+    @friend = Friend.new
   end
+
+  def create
+    @friend = Friend.new(friends_params)
+  
+    if @friend.save
+      redirect_to pages_path
+    else
+      render :new
+    end
+  end
+  
+  def edit
+    @friend = Friend.find(params[:id])
+  end
+  
+  def update
+    @friend = Friend.find(params[:id])
+  
+    if @friend.update(friend_params)
+      redirect_to pages_path
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    Friend.find(params[:id]).destroy
+    redirect_to friends_path
+  end
+    
+  
+  ##@blog.destroy
+    #this only works if 
+    #@blog =  blog.find(params[id]) 
+
+
+  # def create
+  #   @blog = Blog.create(blog_params)
+
+  #   if @blog.save
+  #     redirect_to blogs_path
+  #   else
+  #     render :new
+  #   end
+  # end
+
+  
+  
+  ##########
+  # def edit
+  #   @blog = blog.find(params[id]) 
+  # end
+  
+  
+  # private
+  #   def set blog 
+  #     @blog = blog.find(params[id]) 
+  #   end
+
+  # def blog_params
+  #   params.require(:blog).permit(:title,:body)
+
+  #create and update under private method 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :pages
+root 'friends#index'
+
+resources :pages
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
